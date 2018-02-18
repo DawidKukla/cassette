49358dd224ce71f6b7f4a1a6dd8973a1deca5647
DawidKukla <kukladawid@gmail.com>
2018-02-18 15:50:58 +0100

    Reference test no duplication

---
+++ src/TestSite/Scripts/Test1/Test1.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Test1;
+(function (Test1_1) {
+    var Test1 = /** @class */ (function () {
+        function Test1() {
+        }
+        Test1.prototype.Log = function (msg) {
+            console.log(msg);
+        };
+        return Test1;
+    }());
+    Test1_1.Test1 = Test1;
+    new Test1().Log("Test1");
+})(Test1 = exports.Test1 || (exports.Test1 = {}));
+//# sourceMappingURL=Test1.js.map
---
+++ src/TestSite/Scripts/Test2/Test2.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Test1_1 = require("../Test1/Test1");
+var Test2;
+(function (Test2_1) {
+    var Test2 = /** @class */ (function () {
+        function Test2() {
+        }
+        Test2.prototype.Log = function (msg) {
+            console.log(msg);
+            var dependency = new Test1_1.Test1.Test1().Log("Test 1 inside Test2");
+        };
+        return Test2;
+    }());
+    Test2_1.Test2 = Test2;
+    new Test2().Log("Test2");
+})(Test2 = exports.Test2 || (exports.Test2 = {}));
+//# sourceMappingURL=Test2.js.map
