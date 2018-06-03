a45010af741d9bf4f72fced050677d14059e999d
DawidKukla <kukladawid@gmail.com>
2018-06-03 16:10:48 +0200

    Tests

---
+++ src/TestSite/Scripts/Test1/BClass .ts
@@ -0,0 +1,19 @@
+// @reference ~/Scripts
+export module BClassModule{
+    export class BClass{
+        constructor(){
+           
+        } 
+        
+        public Log(msg:string){
+            var x1=1;
+            var x1=1;
+            var x1=1;
+            var x1=1;
+            var x1=1;
+            console.log(msg)
+        } 
+             
+    } 
+    new BClass().Log("BClass");
+}
--- src/TestSite/Scripts/Test1/Test1.ts
+++ src/TestSite/Scripts/Test1/Test1.ts
@@ -2,9 +2,11 @@
 
 
 import {AClassModule} from "./AClass";
+import {BClassModule} from "./BClass ";
 
 export module Test1{
     import AClass = AClassModule.AClass;
+    import BClass = BClassModule.BClass;
     export class Test1{
         constructor(){
            
@@ -12,6 +14,7 @@ export module Test1{
         
         public Log(msg:string){
             var dep=new AClass();
+            var dep1=new BClass();
             var x1=1;
             var x1=1;
             var x1=1;
