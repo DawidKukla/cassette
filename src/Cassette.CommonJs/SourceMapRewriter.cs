84dd730c2693e8300b742852996dd146ace009a6
DawidKukla <kukladawid@gmail.com>
2018-06-03 14:48:56 +0200

    SourceMapRewrite

--- src/Cassette.CommonJs/SourceMapRewriter.cs
+++ src/Cassette.CommonJs/SourceMapRewriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,14 @@ namespace Cassette.CommonJs
     public class SourceMapRewriter
     {
         const string SOURCE_MAP_DATA_START = "//# sourceMappingURL=data:application/json;base64,";
-        readonly CompositeSourceMap compositeSourceMap = new CompositeSourceMap();
+        readonly CompositeSourceMap _compositeSourceMap = new CompositeSourceMap();
 
         public MemoryStream Rewrite(MemoryStream stream)
         {
             var rewritedStream = new MemoryStream();
 
             var counter = 1;
-            var offset = 0;
+            var lineOffset = 0;
 
             var writer = new StreamWriter(rewritedStream);
             using (var reader = new StreamReader(stream))
@@ -27,22 +28,11 @@ namespace Cassette.CommonJs
                 {
                     if (line.EndsWith(CommonJsWriter.LOCAL_REQUIRE_SCRIPT_START))
                     {
-                        offset = counter;
-                        writer.WriteLine(line);
+                        lineOffset = MarkOffset(counter, writer, line);
                     }
                     else if (line.StartsWith(SOURCE_MAP_DATA_START))
                     {
-                        var data = Convert.FromBase64String(line.Substring(SOURCE_MAP_DATA_START.Length,
-                            line.Length - SOURCE_MAP_DATA_START.Length));
-                        var decodedString = Encoding.UTF8.GetString(data);
-                        var sourceMap = JsonConvert.DeserializeObject<SourceMap>(decodedString);
-
-                        compositeSourceMap.sections.Add(new Section
-                        {
-                            offset = new Offset { line = offset, column = 0 },
-                            map = sourceMap
-                        });
-
+                        ExtractMap(line, lineOffset);
                         RemoveMap(writer);
                     }
                     else
@@ -53,24 +43,65 @@ namespace Cassette.CommonJs
                     counter++;
                 }
 
-                if (compositeSourceMap.sections.Any())
-                {
-                    var lastSection = compositeSourceMap.sections.Last();
-
-                    compositeSourceMap.version = lastSection.map.version;
-                    compositeSourceMap.file = lastSection.map.file;
-                    compositeSourceMap.sections.Select(x => x.map).ToList()
-                        .ForEach(m => { m.file = lastSection.map.file; });
-
-                    var newMapData =
-                        Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(compositeSourceMap)));
-                    writer.WriteLine(SOURCE_MAP_DATA_START + newMapData);
-                }
+                WriteCompositeMap(writer);
             }
 
             writer.Flush();
             rewritedStream.Position = 0;
             return rewritedStream;
+        }
+
+        void WriteCompositeMap(StreamWriter writer)
+        {
+            if (!_compositeSourceMap.sections.Any()) return;
+            RewriteOrginalFileNameUsingLastSection();
+            var serializedSourceMap = SerializeToJSON();
+            var newMapData = EncodeBase64(serializedSourceMap);
+            writer.WriteLine(SOURCE_MAP_DATA_START + newMapData);
+        }
+
+        static string EncodeBase64(string serializedSourceMap)
+        {
+            var newMapData =
+                Convert.ToBase64String(Encoding.UTF8.GetBytes(serializedSourceMap));
+            return newMapData;
+        }
+
+        string SerializeToJSON()
+        {
+            var serializedSourceMap = JsonConvert.SerializeObject(_compositeSourceMap);
+            return serializedSourceMap;
+        }
+
+        void RewriteOrginalFileNameUsingLastSection()
+        {
+            var lastSection = _compositeSourceMap.sections.Last();
+
+            _compositeSourceMap.version = lastSection.map.version;
+            _compositeSourceMap.file = lastSection.map.file;
+            _compositeSourceMap.sections.Select(x => x.map).ToList()
+                .ForEach(m => { m.file = lastSection.map.file; });
+        }
+
+        void ExtractMap(string line, int lineOffset)
+        {
+            var data = Convert.FromBase64String(line.Substring(SOURCE_MAP_DATA_START.Length,
+                line.Length - SOURCE_MAP_DATA_START.Length));
+            var decodedString = Encoding.UTF8.GetString(data);
+            var sourceMap = JsonConvert.DeserializeObject<SourceMap>(decodedString);
+
+            _compositeSourceMap.sections.Add(new Section
+            {
+                offset = new Offset { line = lineOffset, column = 0 },
+                map = sourceMap
+            });
+        }
+
+        static int MarkOffset(int counter, StreamWriter writer, string line)
+        {
+            var lineOffset = counter;
+            writer.WriteLine(line);
+            return lineOffset;
         }
 
         static void RemoveMap(StreamWriter writer)
@@ -84,14 +115,15 @@ namespace Cassette.CommonJs
             public int column { get; set; }
         }
 
+        [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")]
         class SourceMap
         {
             public int version { get; set; }
             public string file { get; set; }
-            public List<string> sources { get; set; }
-            public List<object> names { get; set; }
+            public List<string> sources { get; set; } = new List<string>();
+            public List<object> names { get; set; } = new List<object>();
             public string mappings { get; set; }
-            public List<string> sourcesContent { get; set; }
+            public List<string> sourcesContent { get; set; } = new List<string>();
         }
 
         class Section
@@ -104,7 +136,7 @@ namespace Cassette.CommonJs
         {
             public int version { get; set; }
             public string file { get; set; }
-            public List<Section> sections { get; set; }
+            public List<Section> sections { get; } = new List<Section>();
         }
     }
 }
